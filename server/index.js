94032ee25319d0165a237f3a302c539151478e51
gayatrisathawane <gayatripsathawane@gmail.com>
2023-12-02 12:28:46 +0530

    MongoDb connection

--- server/index.js
+++ server/index.js
@@ -5,8 +5,23 @@ dotenv.config()
 
 const app = express()
 
+app.use(express.json())
+
 const PORT = process.env.PORT || 8080
 
+
+//connection of mongodb 
+
+
+const mongoDb = async()=>{
+
+    const connect = await mongoose.connect(process.env.MONGO_DB)
+
+    if(connect){
+        console.log("mongo db connect ")
+    }
+}
+mongoDb()
 
 
 
