f257709462fd7a70b9f43613d9c5c6c7bd246acb
gayatrisathawane <gayatripsathawane@gmail.com>
2023-12-03 18:32:50 +0530

    home page designed and navbar modified

--- client/src/component/Navbar/Navbar.js
+++ client/src/component/Navbar/Navbar.js
@@ -4,34 +4,41 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-
-
-<nav className="navbar navbar-expand-lg bg-info">
+<nav className="navbar navbar-expand-lg bg-light shadow-sm p-3">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="#">HOSTEL</Link>
+    <Link className="navbar-brand fs-4" to="#">HOSTELBOOK</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-auto">
         <li className="nav-item">
-          <Link className="nav-link active fs-5" aria-current="page" to="#">Home</Link>
+          <Link className="nav-link active fs-4" aria-current="page" to="#">Home</Link>
         </li>
        
         <li className="nav-item">
-          <Link className="nav-link fs-5 active" to="#">Add Student </Link>
+          <Link className="nav-link fs-4 active ms-4" to="#">Rooms</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link fs-5 active" to="/signup">SignUp</Link>
+          <Link className="nav-link fs-4 active ms-4" to="/signup">Contact</Link>
         </li>
+        <li className="nav-item">
+          <Link className="nav-link fs-4 active ms-4" to="/signup">About us </Link>
+        </li>
+        
+      </ul>
+      <button className="btn btn-outline-primary ms-5 p-2 fs-5 ">
+          <Link className="nav-link  active "  to="/login">SignUp</Link>
+        </button><br/><br/>
     
          
-        <li className="nav-item">
-          <Link className="nav-link fs-5 active "  to="/login">Login</Link>
-        </li>
-      </ul>
+        <button className=" btn btn-outline-primary me-5 ms-5  p-2 fs-5 ">
+          <Link className="nav-link  active "  to="/login">Login</Link>
+        </button>
      
     </div>
+
+   
 
       
     
--- client/src/view/Home/Home.js
+++ client/src/view/Home/Home.js
@@ -1,13 +1,38 @@
 import React from 'react'
 import Navbar from '../../component/Navbar/Navbar'
+import './Home.css'
 
 
 const Home = () => {
   return (
     <div>
-       <Navbar/>
-        <h1>Home </h1>
-      
+      <Navbar />
+      <div className='container home-container mt-5 py-5'>
+        <div className=' row'>
+
+          <div className='col-md-6'>
+
+            <div className='ps-3'>
+              <h2>HOSTELBOOK</h2>
+              <p className='fs-2 '>Amazing hostel for students .....</p>
+              <p className='fs-5 text-dark '>A hostel is a lower-priced inn of sorts that offers basic, shared accommodations. Typically, a hostel features a large room with separate beds, a shared bathroom, and a communal kitchen. Some hostels have private rooms, but the lower-cost ones generally offer bunk beds.</p>
+            </div>
+
+          </div>
+
+          <div className='col-md-6'>
+
+            <div className=''>
+
+             <img src="https://www.greenvilleonline.com/gcdn/presto/2021/02/22/PGRE/dfe5ab88-a835-4f9a-873a-d40e7708881c-0321_TG_Modal_02.JPG?crop=2399,1350,x0,y0&width=660&height=372&format=pjpg&auto=webp" alt="hostel-img" className='home-hostel-img'/>
+            </div>
+
+          </div>
+
+
+        </div>
+      </div>
+
     </div>
   )
 }
