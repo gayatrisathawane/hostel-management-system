812c1b0f80b0491dfdea7d1835319fe371046115
kartikkotkar <kartikkotkar973@gmail.com>
2023-12-03 08:38:58 -0800

    added signup file

--- client/src/view/SignUp/SignUp.js
+++ client/src/view/SignUp/SignUp.js
@@ -1,12 +1,166 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-const SignUp = () => {
-  return (
-    <div>
-        <h1>SignUp</h1>
-      
+import axios from 'axios';
+import "./SignUp.css"
+import { Link } from "react-router-dom";
+import Navbar from "../../component/Navbar/Navbar";
+
+
+function Signup() {
+
+  const [name, setNmae] = useState("");
+  const [mobile ,setMobile] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+  const [gender, setGender] = useState("female");
+
+  const signupUser = async () => {
+    if (!name) {
+      alert("Nmae is requred");
+      return;
+    }
+    if (!mobile) {
+      alert("Mobile is requred");
+      return;
+    }
+
+    if (!email) {
+      alert("Email is requred");
+      return;
+    }
+
+    if (!password) {
+      alert("Password is requred");
+      return;
+    }
+
+
+    const response = await axios.post("/api/signup", {
+      name: name,
+      mobile:mobile,
+      email: email,
+      password: password,
+      gender: gender
+    })
+
+    alert(response?.data?.message);
+
+    if (response?.data?.success) {
+
+      window.location.href = "/login";
+    }
+
+
+  };
+
+  useEffect(() => {
+    const storageUser = JSON.parse(localStorage.getItem("user") || '{}');
+
+    if (storageUser?.email) {
+      alert("You are alredy looged in!");
+      window.location.href = "/";
+    }
+
+  }, [])
+
+  return (<>
+  
+  <Navbar/>
+    <div className="main-heading">
+
+      <from className="signup-from">
+        <h1 className="text-center">Sign-Up</h1>
+        <div>
+          <label htmlFor="name" className="text-color">Name</label>
+          <input type="text"
+            placeholder="Enter your name"
+            id="name"
+            className="from-control"
+            value={name}
+            onChange={(e) => {
+              setNmae(e.target.value);
+            }} />
+        </div>
+
+        <div>
+          <label htmlFor="mobile" className="text-color">mobile</label>
+          <input type="text"
+            placeholder="Enter your mobile"
+            id="mobile"
+            className="from-control"
+            value={mobile}
+            onChange={(e) => {
+              setMobile(e.target.value);
+            }} />
+        </div>
+
+        <div>
+          <label htmlFor="email" className="text-color">Email</label>
+          <input type="email"
+            placeholder="Enter your email"
+            id="email"
+            className="from-control"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }} />
+        </div>
+        <div>
+
+        </div>
+
+        <div>
+          <label htmlFor="password" className="text-color">Password</label>
+          <input type="password"
+            placeholder="Enter your password"
+            id="password"
+            className="from-control"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }} />
+
+
+
+          <div>
+
+            <input type="radio"
+              id="male"
+              name="gender"
+              checked={gender === "male"}
+              onClick={() => {
+                setGender("male");
+              }}
+            />
+            <label htmlFor='male' className="text-color">male</label>
+
+
+            <input type="radio"
+              id="femlae"
+              name="gender"
+              checked={gender === "female"}
+              onClick={() => {
+                setGender("female");
+              }}
+            />
+            <label htmlFor='female' className="text-color">female</label>
+          </div>
+
+          <button type="button"
+            className="btn singup-btn"
+            onClick={signupUser}>
+
+            Singup
+          </button>
+
+        </div>
+      </from>
     </div>
-  )
+ 
+  </>
+   )
+
 }
 
-export default SignUp
+
+export default Signup;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 import express  from "express";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+// import User from "./models/user.js";
+import {postApiLogin, postApiSignup} from './controlers/transaction.js'
 dotenv.config()
 
 const app = express()
 
 app.use(express.json())
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 
 //connection of mongodb 
@@ -15,17 +17,26 @@ const PORT = process.env.PORT || 8080
 
 const mongoDb = async()=>{
 
-    const connect = await mongoose.connect(process.env.MONGO_DB)
+    // post signup
+
+    
+
+    const connect = await mongoose.connect(process.env.MONGODB_URI)
 
     if(connect){
-        console.log("mongo db connect ")
+        console.log("mongo db connect successfully ")
     }
 }
 mongoDb()
 
+// post signup
+app.post('/api/signup', postApiSignup )
 
+//post login
+app.post('/api/login', postApiLogin)
 
 app.listen(PORT ,()=>{
-    console.log(`app listen on port ${PORT} `)
-})
+    console.log(`server is running ${PORT} `);
+    
+});
 
