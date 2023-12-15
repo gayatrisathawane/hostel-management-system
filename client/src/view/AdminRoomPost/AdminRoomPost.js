c6b3bac0f5483eb651ea4a86b022036149a778d8
Bandini2001 <bandinikohare30@gmail.com>
2023-12-15 13:56:39 +0530

    add css on admin

--- client/src/view/AdminRoomPost/AdminRoomPost.js
+++ client/src/view/AdminRoomPost/AdminRoomPost.js
@@ -50,14 +50,14 @@ const AdminRoomPost = () => {
         <>
             <div className='admin-page'>
                 <Navbar />
-                <div>
+                <div className='d-flex justify-content-evenly'>
                     <form className='post-room-form'> <div>
                         <h2>Add Room</h2>
                         <label>Title</label><br />
                         <input type="text"
                             placeholder="Enter title here.."
                             id="title"
-                            className="from-control"
+                            className="input"
                             value={title}
                             onChange={(e) => {
                                 setTitle(e.target.value);
@@ -67,7 +67,7 @@ const AdminRoomPost = () => {
                         <div>
                             <label>Add description</label><br />
                             <input type="text"
-                                placeholder="Add descriptin"
+                                placeholder="Add description"
                                 id="description"
                                 className="description-input"
                                 value={description}
@@ -81,7 +81,7 @@ const AdminRoomPost = () => {
                             <input type="number"
                                 placeholder="10000, 2000 etc"
                                 id="price"
-                                className="from-control"
+                                className="input"
                                 value={price}
                                 onChange={(e) => {
                                     setPrice(e.target.value);
@@ -91,7 +91,7 @@ const AdminRoomPost = () => {
                         <label>Select Room Type</label>
                         <br />
                         <select
-                            className='select-box'
+                            className='select-box input'
                             value={type}
                             onChange={(e) => {
                                 setType(e.target.value)
@@ -109,11 +109,11 @@ const AdminRoomPost = () => {
 
 
                         <div><br />
-                            <label>How many Candidates</label><br />
+                            <label>Allow Candidates</label><br />
                             <input type="text"
                                 placeholder="1/2 etc."
                                 id="candidate"
-                                className="from-control"
+                                className="input"
                                 value={candidate}
                                 onChange={(e) => {
                                     setCandidate(e.target.value);
@@ -124,7 +124,7 @@ const AdminRoomPost = () => {
                             <label>Add image url</label><br />
                             <input type="text"
                                 id="image"
-                                className="from-control"
+                                className="input"
                                 value={image}
                                 onChange={(e) => {
                                     setimage(e.target.value);
@@ -140,7 +140,7 @@ const AdminRoomPost = () => {
                             </button>
 
                         </div></form>
-                    <div className='all-links-container'>
+                    <div className='all-links-container mt-5'>
 
                         {
                             rooms?.map((roomObj, index) => {
@@ -153,10 +153,16 @@ const AdminRoomPost = () => {
 
                                 return (
 
-                                    <div className='room-card'>
-                                       <h2>title : {title}</h2>
-                                       <h3>Type : {type}</h3>
-                                       <p>{description}</p>
+                                    <div className='room-card-1 p-2 d-flex justify-content-between mt-4'>
+                                        <div className='img'>
+                                            <img src={image} alt='room-img' className='img'/>
+                                        </div>
+                                       <div className='details'><h4>{title}</h4>
+                                       <h5>Type : {type}</h5>
+                                       <p className='description'>Description : {description}</p>
+                                       <h5>Rent : Ru {price}</h5>
+                                       <h6>Allow Candidate : {candidate}</h6></div>
+                                      
                                     </div>
                                 )
                             })
