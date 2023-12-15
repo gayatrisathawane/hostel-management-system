62ddda749035c06b2e1bd84986134feda1c246ad
gayatrisathawane <gayatripsathawane@gmail.com>
2023-12-15 13:50:08 +0530

    sign up page designed

--- client/src/view/SignUp/SignUp.js
+++ client/src/view/SignUp/SignUp.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./SignUp.css"
 import Navbar from "../../component/Navbar/Navbar";
+import SignUpBg from './SignUpBg.png'
+import { Link } from "react-router-dom";
 
 
 
 
 function Signup() {
 
-  const [name, setNmae] = useState("");
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [mobile, setMobile] = useState('');
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("")
@@ -25,6 +28,11 @@ function Signup() {
       return;
     }
 
+    if (!lastName) {
+      alert("LastName is requred");
+      return;
+    }
+
     if (!email) {
       alert("Email is requred");
       return;
@@ -38,13 +46,12 @@ function Signup() {
 
     const response = await axios.post("/api/signup", {
       name: name,
+      lastName: lastName,
       mobile: mobile,
       email: email,
       password: password,
       gender: gender,
-      image: image,
-    
-
+      image: image
     })
 
     alert(response?.data?.message);
@@ -70,7 +77,184 @@ function Signup() {
   return (
     <>
       <Navbar />
-      <div>
+
+
+      <div className="signup-main-container">
+
+        <div className="signup-img-container">
+
+          <img src={SignUpBg} className="signup-img" />
+
+
+        </div>
+        <div className="sign-up-details">
+
+          <h1 className="sign-up-heading">Welcome to <span className="blue-color">H</span>ostel<span className="blue-color">B</span>ook</h1>
+
+          <div>
+
+            <div className="d-flex justify-content-evenly">
+
+              <div className="p-2">
+
+                <label htmlFor="name" className="input-label"> First Name</label>
+                <input type="text"
+                  placeholder="Enter your name"
+                  id="name"
+                  className="signup-input-field"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }} />
+
+              </div>
+
+
+
+              <div className="p-2">
+
+
+                <label htmlFor="name" className="input-label"> Last Name</label>
+                <input type="text"
+                  placeholder="Enter your name"
+                  id="name"
+                  className="signup-input-field"
+                  value={lastName}
+                  onChange={(e) => {
+                    setLastName(e.target.value);
+                  }} />
+              </div>
+            </div>
+
+
+
+
+
+
+          </div>
+
+
+          <div>
+            <div className="d-flex">
+
+              <div className="p-2">
+                <label htmlFor="mobile" className="input-label" >Mobile</label>
+                <input type="text"
+                  placeholder="Enter your mobile"
+                  id="mobile"
+                  className="signup-input-field"
+                  value={mobile}
+                  onChange={(e) => {
+                    setMobile(e.target.value);
+                  }} />
+              </div>
+
+              <div className="p-2">
+                <label htmlFor="email" className="input-label">Email</label>
+                <input type="email"
+                  placeholder="you@example.com"
+                  id="email"
+                  className="signup-input-field"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }} />
+              </div>
+
+            </div>
+          </div>
+
+
+
+
+          <div>
+            <div className="d-flex">
+
+
+              <div className="p-2">
+
+
+                <label htmlFor="password" className="input-label" >Password</label>
+                <input type="password"
+                  placeholder="Enter 8 characters or mor"
+                  id="password"
+                  className="signup-input-field"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }} />
+
+              </div>
+
+              <div className="p-2">
+                <label htmlFor="image" className="input-label">Image</label>
+                <input type="text"
+                  id="image"
+                  placeholder="Enter your image url"
+                  className="signup-input-field"
+                  value={image}
+                  onChange={(e) => {
+                    setimage(e.target.value);
+                  }} />
+
+              </div>
+
+            </div>
+
+
+
+          </div>
+
+          <div>
+            <div className="d-flex">
+
+              <label className="input-label ms-2">Gender :-</label>
+
+              <div>
+
+                <input type="radio"
+                  id="male"
+                  name="gender"
+                  className="gender-input ms-5"
+                  checked={gender === "male"}
+                  onClick={() => {
+                    setGender("male");
+                  }}
+                />
+                <label htmlFor='male' className="fs-4 ms-2">Male</label>
+
+
+                <input type="radio"
+                  id="femlae"
+                  name="gender"
+                  className="gender-input ms-5"
+                  checked={gender === "female"}
+                  onClick={() => {
+                    setGender("female");
+                  }}
+                />
+                <label htmlFor='female' className="fs-4 ms-2">Female</label>
+              </div>
+
+            </div>
+          </div>
+
+
+
+          <button type="button"
+            className=" singup-btn"
+            onClick={signupUser}>
+            SignUp
+          </button>
+
+          <Link className="text-decoration-none text-center" to='/login'><p className="fs-5">Already have an account ?</p></Link>
+          <div>
+          </div>
+        </div>
+      </div>
+
+
+      {/* <div>
         <div className="signup-container-main">
           <div className="main-container">
 
@@ -183,14 +367,14 @@ function Signup() {
 
           </div>
           </div>
-          </div>
+          </div> */}
 
 
 
-        </>
-        )
+    </>
+  )
 
 }
 
 
-        export default Signup;
+export default Signup;
--- server/models/user.js
+++ server/models/user.js
@@ -6,6 +6,11 @@ const userSchema = new Schema({
         type: String,
         required:true
     },
+    lastname:{
+        type: String,
+        required:true
+
+    },
     mobile: {
         type: String,
         required: true,
